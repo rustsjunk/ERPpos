70883bce1d1a4368495b9a3842e1957de2b24f4d
rustsjunk <rustsjunk@gmail.com>
2025-11-04 08:45:04 +0000

    need to fix login

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+from pos_server import app
+
+if __name__ == '__main__':
+    debug = os.getenv('FLASK_DEBUG', '0') == '1'
+    port = int(os.getenv('PORT', '5000'))
+    host = os.getenv('HOST', '0.0.0.0')
+    app.run(host=host, port=port, debug=debug)
